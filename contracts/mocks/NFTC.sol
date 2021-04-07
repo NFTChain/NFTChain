36b6337740ff9724a1ea8f6d181f34ce271e1e86
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-07 21:09:42 +0200

    delete unnecessary folder

--- contracts/mocks/NFTC.sol
+++ contracts/mocks/NFTC.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.0;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NFTC is ERC20 {
-    constructor() ERC20("NFTC", "NFTChain") {}
-    
-    function faucet(address to, uint amount) external {
-      _mint(to, amount);
+    constructor() public ERC20("NFTC", "NFTChain") {}
+
+    function faucet(address to, uint256 amount) external {
+        _mint(to, amount);
     }
 }
