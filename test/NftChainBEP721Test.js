a706a00341549122ca06f7030897608d70693226
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-23 21:03:59 +0200

    add test for create / buy inks and tokens

--- test/NftChainBEP721Test.js
+++ test/NftChainBEP721Test.js
@@ -13,8 +13,8 @@ describe("BEP721 contract", async () => {
     BEP20Contract = await ethers.getContractFactory("NftChainBEP20");
     BEP20Token = await BEP20Contract.deploy();
 
-    // BEP721Contract = await ethers.getContractFactory("NftChainBEP721");
-    // BEP721Token = await BEP721Contract.deploy(BEP20Token.address);
+    BEP721Contract = await ethers.getContractFactory("NftChainBEP721");
+    BEP721Token = await BEP721Contract.deploy(BEP20Token.address);
     [owner, addr1, addr2, _] = await ethers.getSigners();
   });
 
@@ -22,6 +22,88 @@ describe("BEP721 contract", async () => {
     it("Should assign the total supply of tokens to the owner", async () => {
       const ownerBalance = await BEP20Token.balanceOf(owner.address);
       expect(await BEP20Token.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe("Create inks and mint tokens", () => {
+    it("Should create the first ink", async () => {
+      const createInk = await BEP721Token.createInk("inkurl", 1, 1000);
+      const totalInks = await BEP721Token.totalInks();
+      expect(totalInks).to.equal(1);
+    });
+  });
+
+  describe("Buy inks and tokens", () => {
+    it("Should buy 1 ink and 1 token", async () => {
+      // create 1 ink for addr1
+      const inkUrl = "inkurl";
+      const inkPrice = 1000;
+      const createInk = await BEP721Token.connect(addr1).createInk(
+        inkUrl,
+        1,
+        inkPrice
+      );
+
+      // approve BEP721 address transaction for owner
+      const approveTransaction = await BEP20Token.approve(
+        BEP721Token.address,
+        inkPrice
+      );
+
+      // buy 1 ink for owner
+      const buyInk = await BEP721Token.buyInk(inkUrl); // sends NFTC tokens to artist (addr1) and minted token to buyer (owner)
+
+      // check if ink got created
+      const totalInks = await BEP721Token.totalInks();
+      expect(totalInks).to.equal(1);
+
+      // check if buyer got minted token
+      const firstMintedTokenId = 1;
+      const ownerOfToken = await BEP721Token.ownerOf(firstMintedTokenId);
+      expect(ownerOfToken).to.equal(owner.address);
+
+      // check if seller/artist got NFTC tokens
+      const balanceOfArtist = await BEP20Token.balanceOf(addr1.address);
+      expect(balanceOfArtist).to.equal(inkPrice);
+    });
+
+    it("Should buy 1 token", async () => {
+      const initialBalanceOfOwner = await BEP20Token.balanceOf(owner.address);
+      const createInk = await BEP721Token.connect(addr1).createInk(
+        "inkurl",
+        1,
+        1000
+      );
+      const approveTransaction = await BEP20Token.approve(
+        BEP721Token.address,
+        1000
+      );
+      const buyInk = await BEP721Token.buyInk("inkurl");
+
+      // set token price for owners token
+      const setTokenPrice = await BEP721Token.setTokenPrice(1, 50);
+
+      // approve the transaction for addr1
+      const approve = await BEP20Token.connect(addr1).approve(
+        BEP721Token.address,
+        50
+      );
+
+      // buy with addr1 the minted token from owner
+      const buyToken = await BEP721Token.connect(addr1).buyToken(1);
+
+      // check if addr1 got the minted token
+      const firstMintedTokenId = 1;
+      const ownerOfToken = await BEP721Token.ownerOf(firstMintedTokenId);
+      expect(ownerOfToken).to.equal(addr1.address);
+
+      // check if owner got the NFTC tokens // problems with bigNumber substraction
+      //   const balanceOfOwner = await BEP20Token.balanceOf(owner.address);
+      //   expect(balanceOfOwner).to.equal(initialBalanceOfOwner - 950); // 950 = bought for 1000 and sold for 50
+
+      // check if addr1 paid for the token
+      const balanceOfAddr1 = await BEP20Token.balanceOf(addr1.address);
+      expect(balanceOfAddr1).to.equal(950);
     });
   });
 });
