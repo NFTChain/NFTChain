57a52ec1ce4874637c73909a1e51a1aa1c23eb54
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-06-17 11:08:45 +0200

    deploy to matic mainnet

--- contracts/NftChainBEP20.sol
+++ contracts/NftChainBEP20.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NftChainBEP20 is ERC20 {
     constructor() public ERC20("NFTChain", "NFTC") {
-        _mint(msg.sender, 1000000000000 * 10**18);
+        _mint(msg.sender, 1000000000 * 10**18);
     }
 }
